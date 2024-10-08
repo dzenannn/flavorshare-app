5c70be6406f361a540fea9dfbbd38dbe1fa7a138
dzenann <dzenan.dev@gmail.com>
2024-10-08 17:11:39 +0200

    outputting data

--- app/meals/[placeholder]/page.js
+++ app/meals/[placeholder]/page.js
@@ -1,15 +1,5 @@
 import Link from "next/link";
 
 export default function PlaceholderPage({ params }) {
-  return (
-    <div style={{ textAlign: "center", fontSize: "2rem" }}>
-      Slug stranica
-      <br />
-      <br />
-      {params.placeholder}
-      <br />
-      <br />
-      <Link href={"/"}>Back to Homepage</Link>
-    </div>
-  );
+  return <div>Placeholder</div>;
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,56 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+import pizzaImg from "../../public/images/pizza.jpg";
 
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-      <Link href={"meals/share"}>Share Meal</Link>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}> by Dženan</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid
+          meals={[
+            {
+              id: "meal1",
+              title: "Delicious Pizza",
+              slug: "delicious-pizza",
+              image: pizzaImg,
+              summary:
+                "A mouthwatering pasta dish with rich tomato sauce and fresh herbs.",
+              creator: "Chef Antonio",
+            },
+            {
+              id: "meal2",
+              title: "Burger ",
+              slug: "delicious-Burger",
+              image: "/images/burger.jpg",
+              summary:
+                "A mouthwatering pasta dish with rich tomato sauce and fresh herbs.",
+              creator: "Chef Antonio",
+            },
+            {
+              id: "meal1",
+              title: "Delicious Schnitzel",
+              slug: "delicious-schnitzel",
+              image: "/images/schnitzel.jpg",
+              summary:
+                "A mouthwatering pasta dish with rich tomato sauce and fresh herbs.",
+              creator: "Chef Antonio",
+            },
+          ]}
+        />
+      </main>
+    </>
   );
 }
