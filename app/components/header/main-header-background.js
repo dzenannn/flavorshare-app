880650b4c1bcbe5f9d287b80036159f9c9973c74
dzenann <dzenan.dev@gmail.com>
2024-10-08 15:51:25 +0200

    using client components effectively

--- app/components/header/main-header-background.js
+++ app/components/header/main-header-background.js
@@ -1,6 +1,6 @@
 import classes from "./main-header-background.module.css";
 
-export function MainHeaderBackground() {
+export default function MainHeaderBackground() {
   return (
     <div className={classes["header-background"]}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
--- app/components/header/main-header.js
+++ app/components/header/main-header.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { MainHeaderBackground } from "./main-header-background";
+import MainHeaderBackground from "./main-header-background";
 import logo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">FlavorShare Community</Link>
+              <NavLink href="/community">FlavorShare Community</NavLink>
             </li>
           </ul>
         </nav>
