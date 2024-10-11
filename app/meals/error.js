c5eb3b2d65647e2152620a06786746d851650b59
dzenann <dzenan.dev@gmail.com>
2024-10-11 15:15:55 +0200

    Handling errors (Adding error page)

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h3>An error occurred!</h3>
+      <p>Failed to load meal data, please try again later!</p>
+    </main>
+  );
+}
