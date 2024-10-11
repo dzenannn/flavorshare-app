d12b27c8d2543d2d8be7c8e142baa252aeba9119
dzenann <dzenan.dev@gmail.com>
2024-10-11 15:46:32 +0200

    Loading and rendering meal details via dynamic routes

--- app/meals/error.js
+++ app/meals/error.js
@@ -3,7 +3,7 @@
 export default function Error() {
   return (
     <main className="error">
-      <h3>An error occurred!</h3>
+      <h1>An error occurred!</h1>
       <p>Failed to load meal data, please try again later!</p>
     </main>
   );
